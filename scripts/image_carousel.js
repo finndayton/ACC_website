a7968acdda63527a8bf2794580c3007d8ffac157
Dean Stratakos <dastratakos@gmail.com>
2021-12-03 10:53:36 -0800

    Fix image carousel wrapping!!

--- scripts/image_carousel.js
+++ scripts/image_carousel.js
@@ -5,52 +5,19 @@ const images = document.querySelector("#art-carousel");
 const number_of_cards_by_index = images.children.length - 1,
   middle_card_by_index = Math.floor(number_of_cards_by_index / 2);
 
-var num_cards_on_screen = 2;
+let num_cards_on_screen = 2;
 if (window.innerWidth > 600) num_cards_on_screen = 3;
 if (window.innerWidth > 900) num_cards_on_screen = 4;
 if (window.innerWidth > 1200) num_cards_on_screen = 5;
 
-const card_width = (window.innerWidth - 60) / num_cards_on_screen;
+let card_width = (window.innerWidth - 60) / num_cards_on_screen;
 
 /* Clone cards and compute sizes. */
 (function () {
   const num_cards = images.children.length;
   for (let i = 0; i < num_cards; i++) {
-    images.children[i].style.width = `${card_width}px`;
-
     var clone = images.children[i].cloneNode(true);
     images.appendChild(clone);
-  }
-  const frames = document.getElementsByClassName("gallery-frame");
-  for (let i = 0; i < frames.length; i++) {
-    frames[i].style.width = `${card_width - 40}px`;
-  }
-  const paintings = document.getElementsByClassName("gallery-painting");
-  for (let i = 0; i < paintings.length; i++) {
-    paintings[i].style.width = `${paintings[i].offsetHeight}px`;
-    console.log(`paintings[i].offsetHeight: ${paintings[i].offsetHeight}`);
-  }
-})();
-
-// window.onresize = orderCards;
-
-/* Order cards. */
-(function () {
-  console.log(`num_cards_on_screen: ${num_cards_on_screen}`);
-  console.log(`card_width: ${card_width}`);
-  for (let i = 0; i < images.children.length; i++) {
-    images.children[i].style.transitionDuration = "1s";
-
-    if (i < (images.children.length + num_cards_on_screen) / 2) {
-      images.children[i].style.left = `${30 + i * card_width}px`;
-    } else {
-      images.children[i].style.left = `${
-        30 - (images.children.length - i) * card_width
-      }px`;
-    }
-
-    images.children[i].style.transitionDuration = "0.5s";
-    console.log(`card ${i}: ${images.children[i].style.left}`);
   }
 })();
 
@@ -61,6 +28,63 @@ let first_index = Math.floor(
 let last_index =
   (first_index - 1 + images.children.length) % images.children.length;
 let scroll_in_progress = false;
+
+window.onresize = order_cards;
+
+/* Order cards. */
+function order_cards() {
+  let old_num_cards_on_left = Math.floor(
+    images.children.length - (images.children.length + num_cards_on_screen) / 2
+  );
+  let first_visible =
+    (first_index + old_num_cards_on_left) % images.children.length;
+
+  if (window.innerWidth <= 600) num_cards_on_screen = 2;
+  if (window.innerWidth > 600) num_cards_on_screen = 3;
+  if (window.innerWidth > 900) num_cards_on_screen = 4;
+  if (window.innerWidth > 1200) num_cards_on_screen = 5;
+
+  card_width = (window.innerWidth - 60) / num_cards_on_screen;
+
+  let num_cards_on_left = Math.floor(
+    images.children.length - (images.children.length + num_cards_on_screen) / 2
+  );
+  first_index =
+    (first_visible + images.children.length - num_cards_on_left) %
+    images.children.length;
+  last_index =
+    (first_index - 1 + images.children.length) % images.children.length;
+
+  for (let index = 0; index < images.children.length; index++) {
+    let i = (index + first_visible) % images.children.length;
+    
+    images.children[i].style.width = `${card_width}px`;
+    images.children[first_index].classList.add("notransition"); // Disable transitions
+    
+    if (index < (images.children.length + num_cards_on_screen) / 2) {
+      images.children[i].style.left = `${30 + index * card_width}px`;
+    } else {
+      images.children[i].style.left = `${
+        30 - (images.children.length - index) * card_width
+      }px`;
+    }
+
+    images.children[first_index].offsetHeight; // Trigger a reflow, flushing the CSS changes
+    images.children[first_index].classList.remove("notransition"); // Re-enable transitions
+
+    console.log(`card ${i}: ${images.children[i].style.left}`);
+  }
+  const frames = document.getElementsByClassName("gallery-frame");
+  for (let i = 0; i < frames.length; i++) {
+    frames[i].style.width = `${card_width - 40}px`;
+  }
+  const paintings = document.getElementsByClassName("gallery-painting");
+  for (let i = 0; i < paintings.length; i++) {
+    paintings[i].style.width = `${paintings[i].offsetHeight}px`;
+    console.log(`paintings[i].offsetHeight: ${paintings[i].offsetHeight}`);
+  }
+}
+order_cards();
 
 /* ************************* BUTTON NAVIGATION ************************* */
 
